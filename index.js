8bf16198ad6e522568a7f440c52725b74bdb890d
CasperFeehily <159107769+Casper-Feehily@users.noreply.github.com>
2024-02-20 23:43:36 +0800

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const notch = require('bindings')('notch.node')
-
-module.exports = {
-  getAllDisplays: notch.getAllDisplays,
-  getDisplayByID: notch.getDisplayByID,
-  safeAreaInsets: notch.safeAreaInsets,
-  auxiliaryTopLeftArea: notch.auxiliaryTopLeftArea,
-  auxiliaryTopRightArea: notch.auxiliaryTopRightArea,
-}
